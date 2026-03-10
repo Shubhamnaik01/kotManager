64d2ea7448ed713d9d96860dfcd5a54a9afe5fe3
Shubhamnaik01 <shubhamnaik4488@gmail.com>
2026-03-10 23:53:04 +0530

    feat(auth): implement secure registration and login with JWT

---
+++ backend/controllers/authController.js
@@ -0,0 +1,49 @@
+import User from "../models/Users.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const saltRound = 10;
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(409).json({ message: "User already exisits" });
+    }
+    const hashedPassword = await bcrypt.hash(password, saltRound);
+
+    const result = await User.create({ name, email, password: hashedPassword });
+    res.status(201).json({ message: "User Registered", user_id: result._id });
+  } catch (error) {
+    console.log("Error while creating the user in server :", error.message);
+    res.status(500).send("Internal server error");
+  }
+};
+
+export const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userExits = await User.findOne({ email });
+    if (!userExits) {
+      return res.status(400).send({ message: "Invalid User" });
+    }
+    const passMatch = await bcrypt.compare(password, userExits.password);
+    if (!passMatch) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+    const token = jwt.sign(
+      { id: userExits._id, role: userExits.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+    return res.status(200).json({
+      message: "User login successfull",
+      token,
+      user: { id: userExits._id, name: userExits.name, role: userExits.role },
+    });
+  } catch (error) {
+    console.log("Error in Server while logging in", error.message);
+    res.status(500).send("Internal server error");
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log("Server running on port :", port);
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log("Server running on port :", port);
+  });
 });
