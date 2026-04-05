02ee115b11367367bd58617a5faba8f95368789a
Shubhamnaik01 <shubhamnaik4488@gmail.com>
2026-04-05 17:24:00 +0530

    feat: backend delete User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,9 +4,8 @@ export const changeRole = async (req, res) => {
   try {
     const _id = req.params.id;
     const { newRole } = req.body;
-    const userExist = await userModel.findById(_id);
-    if (!userExist) {
-      res.status(404).json({ message: "User Not found" });
+    if (!newRole || typeof newRole !== "string") {
+      return res.status(400).json({ message: "Invalid Role" });
     }
     const result = await userModel.findByIdAndUpdate(
       _id,
@@ -15,13 +14,35 @@ export const changeRole = async (req, res) => {
       },
       { new: true, runValidators: true },
     );
-    res.status(200).json({ message: "User Role Updated" });
+    if (!result) {
+      return res.status(404).json({ message: "User Not found" });
+    }
+    return res.status(200).json({ message: "User Role Updated" });
   } catch (error) {
     if (error.name == "ValidationError") {
       const msg = Object.values(error.errors).map((err) => err.message);
       return res.status(400).json({ message: msg[0] });
+    } else if (error.name == "CastError") {
+      return res.status(400).json({ message: "Invalid ID" });
     }
     console.log("Error in server while updating role:", error.message);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const deletedUser = await userModel.findByIdAndDelete(_id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    if (error.name == "CastError") {
+      return res.status(400).json({ message: "Invalid ID" });
+    }
+    console.log("Error in server while deleting user:", error.message);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { changeRole } from "../controllers/userController.js";
+import { changeRole, deleteUser } from "../controllers/userController.js";
 import { isAdmin } from "../middelware/role.middelware.js";
 
 const router = new express.Router();
 
 router.put("/updateRole/:id", isAdmin, changeRole);
+router.delete("/delete/:id", isAdmin, deleteUser);
 
 export default router;
